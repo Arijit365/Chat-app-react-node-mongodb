6c32b9f290324ac28033f110d94d1ce31c9c22ad
Arijit 365 <banerjeearijit365@fmail.com>
2024-07-09 00:44:26 +0530

    work in login.jsx page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,19 @@
 import React , {useState} from 'react';
-import {Container,Paper, Typography} from '@mui/material';
-
+import {Avatar, Button, Container,IconButton,Paper, Stack, TextField, Typography} from '@mui/material';
+import {CameraAlt} from '@mui/icons-material'
 
 export default function Login() {
 
   const [isLogin , setIsLogin] = useState("true");
 
-
+  const toggleLogin = () => setIsLogin((prev) => !prev );
   return (
-    <Container component={"main"} maxWidth={"sx"}>
+    <Container component={"main"} maxWidth={"xs"} sx={{
+      height:"100vh",
+      display:"flex",
+      alignItems:"center",
+      justifyContent:"center"
+    }}>
 <Paper
 elevation={3}
 sx={{
@@ -24,10 +29,119 @@ sx={{
   <>
   {/* Create a Login Form */}
   <Typography variant='h5'> Login  </Typography>
-  <form>
-    
+  <form style={{
+    width:"100%",
+    marginTop:"1rem",
+
+  }}>
+    <TextField 
+    required 
+    fullWidth
+    label="Username"
+    margin="normal"
+    variant="outlined"
+    />
+    <TextField 
+    required 
+    fullWidth
+    label="Password"
+    margin="normal"
+    variant="outlined"
+    />
+    <Button
+    sx={{
+      margin:"1rem"
+    }}
+    variant="contained"
+    color="primary"
+    type="submit"
+    fullWidth
+    >
+      Login
+    </Button>
+    <Typography textAlign="center" m="1rem">OR</Typography>
+    <Button
+    variant="contained"
+    color="primary"
+    type="submit"
+    fullWidth
+    onClick={toggleLogin}
+    >
+      Register Instead
+    </Button>
   </form>
-  </> : <span> Register </span>
+  </> : <>
+  {/* Create Registration page  */}
+  <Typography variant='h5'> Register  </Typography>
+  <form style={{
+    width:"100%",
+    marginTop:"1rem",
+
+  }}>
+    <Stack position="relative" width="10rem" margin="auto">
+<Avatar sx={{
+  width:"10rem",
+  height:"10rem",
+  objectFit:"contain"
+}}/>
+<IconButton>
+  <>
+  <CameraAlt/>
+  </>
+</IconButton>
+    </Stack>
+
+    <TextField 
+    required 
+    fullWidth
+    label="Name"
+    margin="normal"
+    variant="outlined"
+    />
+    <TextField 
+    required 
+    fullWidth
+    label="Username"
+    margin="normal"
+    variant="outlined"
+    />
+    <TextField 
+    required 
+    fullWidth
+    label="Bio"
+    margin="normal"
+    variant="outlined"
+    />
+    <TextField 
+    required 
+    fullWidth
+    label="Password"
+    margin="normal"
+    variant="outlined"
+    />
+    <Button
+    sx={{
+      margin:"1rem"
+    }}
+    variant="contained"
+    color="primary"
+    type="submit"
+    fullWidth
+    >
+      Sign Up
+    </Button>
+    <Typography textAlign="center" m="1rem">OR</Typography>
+    <Button
+    variant="contained"
+    color="primary"
+    type="submit"
+    fullWidth
+    onClick={toggleLogin}
+    >
+      Login Instead
+    </Button>
+  </form>
+  </>
 }
 </Paper>
     </Container>
